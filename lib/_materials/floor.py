b91b93cae9f4f3fa137992ca5cd87fe06bd28bf4
Symon  Kipkemei <85394343+symonkipkemei@users.noreply.github.com>
2023-06-04 06:18:00 +0300

    updates on material calculations

--- lib/_materials/floor.py
+++ lib/_materials/floor.py
@@ -1,12 +1,66 @@
 
 
-def get_brc_mesh(floor_area):
-    pass
+
+
+# Converting units from m to mm for high level accuracy results
+# all units intercepted from the model are
+# length == mm
+#   area == m2
+# volume == m3
+
+def convert_length(units):
+    """
+    converts length from m to mm
+    :param units: units in m
+    :return: units in mm
+    """
+    return units * 1000
+
+def convert_area(units):
+    """
+    convert area from m2 to mm2
+    :param units:
+    :return:
+    """
+    return units * 1000 * 1000
+
+
+def convert_volume(units):
+    """
+    Convert volume from m3 to mm3
+    :param units:
+    :return:
+    """
+    return units * 1000 * 1000 * 1000
+
+
+# get quantities of trucks/lorries/tippers/wheel-barrows
+
+def volume_tipper():
+    """
+    Calculates the volume of a tipper, define the size of a tipper in the manual
+    :return: the volume of a tipper in m3
+    """
+    length = 5.1
+    width = 2.3
+    height = 1.2
+
+    area = length * width
+    volume = area * height
+
+    return volume
+
+
+
+
+
+# all abstraction calculations are done in mm
 
 
 def get_coarse_soft_cement_vol(concrete_mixing_ratio,volume):
-    #type: (tuple,float) -> tuple
+    # type: (tuple,float) -> tuple
     """
+    break down the total volume of a ground slab into the subsequent cement, soft- agg and coarse -agg ratio
     :param concrete_mixing_ratio: ratio of cement, soft_agg, coarse_agg
     :param volume: volume of the concrete in m3
     :return: respective volume of cement,soft_agg, coarse_agg
@@ -16,8 +70,6 @@ def get_coarse_soft_cement_vol(concrete_mixing_ratio,volume):
     coarse_agg_ratio = concrete_mixing_ratio[2]
     concrete_ratio = cement_ratio + soft_agg_ratio + coarse_agg_ratio
 
-    volume = volume * 1000000000
-
     cement_volume = round((cement_ratio/concrete_ratio) * volume, 2)
     soft_agg_volume = round((soft_agg_ratio/concrete_ratio) * volume, 2)
     coarse_agg_volume = round((coarse_agg_ratio/concrete_ratio) * volume, 2)
@@ -25,7 +77,7 @@ def get_coarse_soft_cement_vol(concrete_mixing_ratio,volume):
     return cement_volume, soft_agg_volume, coarse_agg_volume
 
 
-def get_cement_units(cement_volume):
+def get_cement_bags_units(cement_volume):
     """ Abstract number of cement bags from a given volume of cement
     :param cement_volume: Volume of cement in m3
     :return: Number of  50kg bags
@@ -33,22 +85,33 @@ def get_cement_units(cement_volume):
     # approx density of cement kg/m3
     cement_density = 1440
 
-    # 1m3 = 1440kg
-    # how about a 50kg bg
+    # the mass of a cement bag
+    cement_bag_kg = 50
 
-    volume_of_50kg_bag = 50/1440
+    # the volume of a cement bag
+    volume_of_50kg_bag = cement_bag_kg/cement_density
+
+    # no of bags based on volume
     no_of_bags = cement_volume/volume_of_50kg_bag
 
-    return no_of_bags
+    return int(no_of_bags)
 
 
-def get_coarse_units():
-    pass
+def get_aggregate_tippers(agg_volume, tipper_volume):
+    """
+    Abstract the number of tippers needed to construct a ground slab
+    :param agg_volume: The total volume of aggregate needed
+    :param tipper_volume: The total volume of a single tipper
+    :return: The number of tippers needed
+    """
 
+    no_tippers = agg_volume / tipper_volume
 
-
-def get_soft_units():
-    pass
+    if no_tippers < 1:
+        less_volume = agg_volume - tipper_volume
+        print ("The quantities needed are less than volume needed by {} m3".format(less_volume))
+    else:
+        return no_tippers
 
 
 def get_brc_mesh(floor_area_mm):
@@ -72,20 +135,8 @@ def get_brc_mesh(floor_area_mm):
 def get_dpm():
     pass
 
-def convert_to_mm(units):
-    """
-    converts units to mm
-    :param units: units in m
-    :return: units in mm
-    """
-    return  units * 1000
-def convert_to_mm2(units):
-    """
-    convert m2 to mm2
-    :param units:
-    :return:
-    """
-    return  units * 1000 * 1000
+
+
 
 def main():
     conc_mix_ratio = (1, 2, 4)
